c3dcc16065cddcf01b38657eab9311320ffac8bf
Vladimir <Sincere.V.Ch@gmail.com>
2024-08-16 16:09:14 +0300

    resetting written code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
-// import { createElement } from "react";
 
 export const App = () => {
-	// const currentYear = Math.trunc(Date.now() / 1000 / 60 / 60 / 24 / 365) + 1970;
-	const currentYear = new Date().getFullYear();
 	return (
 		<div className="App">
 			<header className="App-header">
@@ -20,44 +17,7 @@ export const App = () => {
 				>
 					Learn React
 				</a>
-				<p>{currentYear}</p>
 			</header>
 		</div>
 	);
 };
-
-// export const App = () => {
-// 	// const currentYear = Math.trunc(Date.now() / 1000 / 60 / 60 / 24 / 365) + 1970;
-// 	const currentYear = new Date().getFullYear();
-// 	return createElement(
-// 		"div",
-// 		{ className: "App" },
-// 		createElement(
-// 			"header",
-// 			{ className: "App-header" },
-// 			createElement("img", {
-// 				src: logo,
-// 				className: "App-logo",
-// 				alt: "logo",
-// 			}),
-// 			createElement(
-// 				"p",
-// 				null,
-// 				"Edit",
-// 				createElement("code", null, "src/App.js"),
-// 				"and save to reload.",
-// 			),
-// 			createElement(
-// 				"a",
-// 				{
-// 					className: "App-link",
-// 					href: "https://reactjs.org",
-// 					target: "_blank",
-// 					rel: "noopener noreferrer",
-// 				},
-// 				"Learn React",
-// 			),
-// 			createElement("p", null, currentYear),
-// 		),
-// 	);
-// };
